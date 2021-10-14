a624d41c8c99075eab7a1b4378f89a814c9aaaff
ahfrenzir <strangername02@gmail.com>
2021-10-14 19:07:13 -0300

    correcciones

--- Controller/GeneralController.php
+++ Controller/GeneralController.php
@@ -5,54 +5,45 @@ require_once "View/GeneralView.php";
 require_once "Helpers/AuthHelper.php";
 
 
-class GeneralController{
+class GeneralController
+{
 
     private $view;
     private $rollsController;
     private $champController;
     private $authHelper;
 
-    function __construct(){
+    function __construct()
+    {
         $this->view = new GeneralView();
         $this->rollsController = new RollsController();
         $this->champController = new ChampionsController();
         $this->authHelper = new AuthHelper();
     }
 
-    function showHome(){
+    function showHome()
+    {
         $this->view->renderHome();
     }
 
-    function showChampions(){
+    function showChampions()
+    {
         $champions = $this->champController->getChampions();
-        $rolls = $this->rollsController-> getRolls();
+        $rolls = $this->rollsController->getRolls();
         $this->view->renderChampionList($champions, $rolls);
     }
 
-    function showChampsByRoll($id){
+    function showChampsByRoll($id)
+    {
         $champions = $this->champController->getChampionsByRoll($id);
-        $roll = $this->rollsController-> getRollForChampion($id);
+        $roll = $this->rollsController->getRollForChampion($id);
         $this->view->renderChampionsByRoll($champions, $roll);
-    }
-
-    function updateChampion(){
-        $this->checkLoggedIn();
-        if(!empty($_POST['name']) && !empty($_POST['description']) && !empty($_POST['history'])){
-            $id = $_POST['id_pj'];
-            $name = $_POST['name'];
-            $description = $_POST['description'];
-            $history = $_POST['history'];
-            $roll = $_POST['id_roll'];
-            $this->champController->updateChampion($name, $description, $history, $roll, $id);
-        }else{
-            $this->view->showError();
-        }
-    }
-
-    function checkLoggedIn(){
-        $this->authHelper->checkLoggedIn();
     }
 
     
 
+    function checkLoggedIn()
+    {
+        $this->authHelper->checkLoggedIn();
+    }
 }
--- View/GeneralView.php
+++ View/GeneralView.php
@@ -2,34 +2,39 @@
 
 require_once 'libs/smarty-3.1.39/smarty-3.1.39/libs/Smarty.class.php';
 
-class GeneralView{
+class GeneralView
+{
 
     private $smarty;
 
-    function __construct(){
-        $this->smarty= new Smarty();
+    function __construct()
+    {
+        $this->smarty = new Smarty();
     }
 
-    function renderHome(){
+    function renderHome()
+    {
         $this->smarty->display('templates/home.tpl');
     }
 
-    function renderChampionList($champions, $rolls){
+    function renderChampionList($champions, $rolls)
+    {
         $this->smarty->assign('titulo', "Lista de campeones");
         $this->smarty->assign('champions', $champions);
         $this->smarty->assign('rolls', $rolls);
         $this->smarty->display('templates/championsList.tpl');
-
     }
 
-    function renderChampionsByRoll($champions, $rolls){
+    function renderChampionsByRoll($champions, $rolls)
+    {
         $this->smarty->assign('titulo', "Lista de campeones: ");
         $this->smarty->assign('roll', $rolls);
         $this->smarty->assign('champions', $champions);
         $this->smarty->display('templates/championsByRoll.tpl');
     }
-    
-    function showError(){
+
+    function showError()
+    {
         $this->smarty->display('templates/error.tpl');
     }
 }
